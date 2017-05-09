81fd6f61cabf331c9ee02ff91e99d49d8b8cf184
Ekaterina_Chub <muron_89@mail.ru>
2017-05-09 18:19:21 +0300

    1fibona44i

---
+++ lesson2/1fibona44i.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+# Написать функцию, принимаюшую на вход число и возвращаюшую последовательность чисел Фибоначчи до этого числа
+
+n = int(input())
+
+def fib(n):
+    f = []
+    f.append(0)
+    f.append(1)
+    i = 3
+    while True:
+        if (f[i - 2] + f[i - 3]) <= n:
+            f.append(f[i-2] + f[i-3])
+        else:
+            break
+        i += 1
+    return f
+
+#print(fib(n))
