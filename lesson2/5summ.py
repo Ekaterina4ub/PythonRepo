3c3da4da88f7b25e238c3c3a395912df66e697f6
Ekaterina_Chub <muron_89@mail.ru>
2017-05-09 20:19:35 +0300

    5summ

--- lesson2/5summ.py
+++ lesson2/5summ.py
@@ -2,3 +2,14 @@
 
 # 5. Написать функцию, которой можно передавать аргументы либо списком/кортежем, либо по одному. Функция производит суммирование всех аргументов
 
+def summ(args):
+    s = 0
+    if isinstance(args, (list, tuple)):
+        s += sum(args)
+    elif isinstance(args, int):
+        s += args
+    return s
+
+# print(summ((12, 13)))
+# print(summ([12, 13]))
+# print(summ(12))
