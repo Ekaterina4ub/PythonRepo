61374aa448bbaab0664592974ca1ed340fdcba4f
Екатерина Чуб <eachub@domclick.ru>
2017-06-07 14:53:48 +0300

    add todolist and dict

--- lesson3/4classdict.py
+++ lesson3/4classdict.py
@@ -1,3 +1,33 @@
 #coding: utf-8
 
 # 4. Написать класс, который бы по всем внешним признакам был бы словарем, но позволял обращаться к ключам как к атрибутам
+
+class MyDict(dict):
+    """
+    >>> x = MyDict([('one', 1), ('two', 2), ('three', 3)])
+    >>> print(x['three'])
+    3
+    >>> print(x.get('two'))
+    2
+    >>> print(x.one)
+    1
+    >>> print(x.test)
+    Traceback (most recent call last):
+    ...
+    AttributeError
+    """
+    def __getattr__(self, name):
+        """
+        перехватываем обращение атрибута которого нет в классе
+        :param name: имя атрибута
+        :return: значение по ключу или ошибку
+        """
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
