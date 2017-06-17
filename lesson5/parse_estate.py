516e2b19e2467a3b2c43ab815fa2dd6fcb87351d
Ekaterina_Chub <muron_89@mail.ru>
2017-06-17 22:59:32 +0300

    parse_estate

--- lesson5/parse_estate.py
+++ lesson5/parse_estate.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 
 # 1. парсим первые N страниц недвижки у domclik, по аналогии с примером который кидался. считаем средню цену на всех страницах
 # 2. пишем генератор который парсит первые N страниц и на каждый шаг возврщает среднюю цену на каждой странице
@@ -7,58 +7,41 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-n = input()
-offset = []
-
-for i in range(n):
-    offset.append(i*30)
-print(offset)
-
+n = int(input())
 price_all_pages = []
-
 base_url = "https://realty.domclick.ru/prodazha-kvartir/?addresses=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&offset=%s&region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&with_photo=1"
-for url in [base_url % i for i in offset]:
-    r = requests.get(url)
+offset = 0
+
+def parse_gen(offset):
+    r = requests.get(base_url % offset)
     html = r.text
-    #print(html)
+    # print(html)
 
     soup = BeautifulSoup(html, features='html.parser')
-    #print(soup.prettify())
+    # print(soup.prettify())
 
     prices = soup.find_all(class_='offer-snippet__title')
-    #print(prices)
+    # print(prices)
 
     res_pr = []
     for pr in prices:
         sp = pr.find('span')
         # удаляем пробелы в цене
         res_pr.append(int(sp.text.replace(' ', '')))
-    #print(res_pr)
+        # print(res_pr)
 
     # посчитаем среднюю цену на странице
-    sum(res_pr)
-    avg_price = sum(res_pr)/len(res_pr)
+    avg_price = int(sum(res_pr) / len(res_pr))
     print(avg_price)
+    yield avg_price
 
-    price_all_pages.append(avg_price)
+
+for i in range(n):
+    for j in parse_gen(offset):
+        price_all_pages.append(j)
+        offset += 30
 
 print(price_all_pages)
 sum(price_all_pages)
 sum_price_all_pages = sum(price_all_pages) / len(price_all_pages)
 print(sum_price_all_pages)
-
-# res = requests.get('https://realty.domclick.ru/prodazha-kvartir/?with_photo=1&addresses=0c5b2444-70a0-4932-980c-b4dc0d3f02b5&region=0c5b2444-70a0-4932-980c-b4dc0d3f02b5')
-# html = res.text
-
-# base_url = "http://example.com/something/?page=%s"
-# for url in [base_url % i for i in xrange(10)]:
-#     r = requests.get(url)
-
-
-# def parse_gen():
-#     for i in range(n):
-#         yield a
-#
-#
-# for i in parse_gen():
-#     print(i)
